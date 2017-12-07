c560be3543c4ef9af435420631d3fe4150671bd0
ChoiJuhye <mac@Appleui-MacBook-Pro.local>
2017-12-08 00:44:32 +0900

    fix packages.json

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,10 @@
-var cool = require('cool-ascii-faces');
 var express = require('express');
-var app = express();
+var router = express.Router();
 
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index')
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
 });
 
-app.get('/cool', function(request, response) {
-  response.send(cool());
-});
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+module.exports = router;
