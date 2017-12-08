2c47638b19938627155dc7d059fd6aa2f5e5e763
ChoiJuhye <mac@Appleui-MacBook-Pro.local>
2017-12-09 00:40:53 +0900

    4

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,25 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const type= [
+  "Type is not set", "Appearance or Singing", "Attraction", "Camp, Trip or Retreat", "Class, Training or Workshop", "Concert or Performance", "Conference", "Convention", "Dinner or Gala", "Festival or Fair", "Game or Competition", "Meeting or Networking Event", "Othor", "Party or Social Gathering", "Race or Endurance Event", "Rally", "Screening", "Seminor or Talk", "Tour", "Tourment"
+];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+router.get('/suggest', (req, res, next) => {
+  let q = req.query.q ? req.query.q.toLowerCase() : '';
+  
+  if (!q) {
+    return res.json([]);
+  }
+
+  res.json(type.filter(name => {
+    return name.toLowerCase().indexOf(q) > -1;
+  }))
+});
 
 module.exports = router;
