e72028356f3b613194f75a72d7ef6303db78d120
Choi <semvoce@naver.com>
2017-12-09 16:11:18 +0900

    9

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var methodOverride = require('method-override');
 var flash = require('connect-flash');
 var mongoose   = require('mongoose');
 var passport = require('passport');
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 var questions = require('./routes/questions');
@@ -46,7 +47,6 @@ app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(cookieParser());
 
 // _method를 통해서 method를 변경할 수 있도록 함. PUT이나 DELETE를 사용할 수 있도록.
@@ -63,7 +63,6 @@ app.use(sassMiddleware({
 
 // session을 사용할 수 있도록.
 app.use(session({
-  name: 'Eventbrite',
   resave: true,
   saveUninitialized: true,
   secret: 'long-long-long-secret-string-1313513tefgwdsvbjkvasd'
@@ -93,7 +92,6 @@ app.use('/', index);
 app.use('/users', users);
 app.use('/questions', questions);
 require('./routes/auth')(app, passport);
-app.use('/api', require('./routes/api'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
